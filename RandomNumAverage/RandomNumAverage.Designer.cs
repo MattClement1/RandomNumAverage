d563de075215ca4532f46056cb54b5d78d764ac9
MattClement1 <matthew.clement1@ocsbstudent.ca>
2015-11-11 14:45:05 -0500

    Random Number Array

---
+++ RandomNumAverage/RandomNumAverage.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace RandomNumAverage
+{
+    partial class frmRandomNumAverage
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblInstructions = new System.Windows.Forms.Label();
+            this.btnClick = new System.Windows.Forms.Button();
+            this.lstNumList = new System.Windows.Forms.ListBox();
+            this.lblNumbersUsed = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblInstructions
+            // 
+            this.lblInstructions.AutoSize = true;
+            this.lblInstructions.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblInstructions.Location = new System.Drawing.Point(12, 9);
+            this.lblInstructions.Name = "lblInstructions";
+            this.lblInstructions.Size = new System.Drawing.Size(348, 16);
+            this.lblInstructions.TabIndex = 0;
+            this.lblInstructions.Text = "Click the button to find the average of 10 random numbers:";
+            // 
+            // btnClick
+            // 
+            this.btnClick.Location = new System.Drawing.Point(147, 37);
+            this.btnClick.Name = "btnClick";
+            this.btnClick.Size = new System.Drawing.Size(75, 23);
+            this.btnClick.TabIndex = 1;
+            this.btnClick.Text = "Click Me";
+            this.btnClick.UseVisualStyleBackColor = true;
+            this.btnClick.Click += new System.EventHandler(this.btnClick_Click);
+            // 
+            // lstNumList
+            // 
+            this.lstNumList.FormattingEnabled = true;
+            this.lstNumList.Location = new System.Drawing.Point(124, 92);
+            this.lstNumList.Name = "lstNumList";
+            this.lstNumList.Size = new System.Drawing.Size(120, 134);
+            this.lstNumList.TabIndex = 2;
+            // 
+            // lblNumbersUsed
+            // 
+            this.lblNumbersUsed.AutoSize = true;
+            this.lblNumbersUsed.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNumbersUsed.Location = new System.Drawing.Point(135, 73);
+            this.lblNumbersUsed.Name = "lblNumbersUsed";
+            this.lblNumbersUsed.Size = new System.Drawing.Size(99, 16);
+            this.lblNumbersUsed.TabIndex = 3;
+            this.lblNumbersUsed.Text = "Numbers used:";
+            // 
+            // frmRandomNumAverage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(368, 238);
+            this.Controls.Add(this.lblNumbersUsed);
+            this.Controls.Add(this.lstNumList);
+            this.Controls.Add(this.btnClick);
+            this.Controls.Add(this.lblInstructions);
+            this.Name = "frmRandomNumAverage";
+            this.Text = "Random Number Average";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblInstructions;
+        private System.Windows.Forms.Button btnClick;
+        private System.Windows.Forms.ListBox lstNumList;
+        private System.Windows.Forms.Label lblNumbersUsed;
+    }
+}
+
---
+++ RandomNumAverage/RandomNumAverage.cs
@@ -0,0 +1,52 @@
+﻿/*
+* Created by: Matthew Clement
+* Created on: 11-Nov-2015
+* Created for: Unit #5-01
+*This program generates 10 random numbers and finds the average
+*/
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RandomNumAverage
+{
+    public partial class frmRandomNumAverage : Form
+    {
+        int[] arrayOfNumbers = new int[11];
+
+        public frmRandomNumAverage()
+        {
+            InitializeComponent();
+        }
+
+        private void btnClick_Click(object sender, EventArgs e)
+        {
+            int counter = 0;
+            double average = 0;
+            this.lstNumList.Items.Clear();
+            Random rnd = new Random();
+
+            while (counter < 10)
+            {   
+                int num = rnd.Next(1, 11);
+
+                arrayOfNumbers[counter] = num;
+
+                this.lstNumList.Items.Add(num);
+
+                average = average + num;
+
+                counter++;
+            }
+            average = average / 10;
+            MessageBox.Show("The average is: " + average);
+        }
+    }
+}
